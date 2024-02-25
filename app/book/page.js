612a1178211b7f0b783d0c848109b3d3140c0029
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2024-02-25 01:23:12 -0500

    Refactor Book component and add metadata

--- app/book/page.js
+++ app/book/page.js
@@ -1,11 +1,28 @@
-import Appointment from '@/components/Appointment'
-import React from 'react'
+import Appointment from "@/components/Appointment";
+import React from "react";
+import { getSEOTags } from "@/libs/seo";
+
+export const metadata = getSEOTags({
+  title: "Book an Appointment",
+  description: "Book an appointment with us to get the best service",
+  canonicalUrlRelative: "/book",
+  keywords: [
+    "book",
+    "appointment",
+    "haircut",
+    "hair color",
+    "hair styling",
+    "hair treatment",
+    "hair care",
+    "Greenwood haircut",
+  ],
+});
 
 export default function Book() {
   return (
-    <div className='container mx-auto my-16'>
-        <h1 className='text-4xl font-bold text-center'>Get an Appointment</h1>
-        <Appointment />
+    <div className="container mx-auto my-16">
+      <h1 className="text-4xl font-bold text-center">Get an Appointment</h1>
+      <Appointment />
     </div>
-  )
+  );
 }
