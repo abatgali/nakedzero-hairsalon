b7e94f5a721ea76d2b0094d1c5fcb8ef1d81f36f
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2024-07-15 20:33:42 -0400

    updated guy's photo

--- components/menandwomencut.js
+++ components/menandwomencut.js
@@ -40,7 +40,7 @@ export default function HaircutforMenandWomen() {
           </div>
           <img
             className="w-64 sm:w-96"
-            src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/mn.png"
+            src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/man_new.png"
             alt="texture"
             layout="fill"
             objectFit="cover"
