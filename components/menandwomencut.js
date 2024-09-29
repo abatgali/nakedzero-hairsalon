f4854890815f6c705b01de66ab657bfb0d73cc55
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2024-09-29 17:32:58 -0400

    fix: haircut pricing section responsiveness

--- components/menandwomencut.js
+++ components/menandwomencut.js
@@ -4,7 +4,7 @@ export default function HaircutforMenandWomen() {
   return (
     <div className="w-full h-auto">
       <div className="bg-gray-950 ">
-        <div className="bg-transparent w-full sm:w-3/4 mx-auto h-auto pt-24 flex flex-row items-start justify-around  max-w-7xl">
+        <div className="bg-transparent w-full md:w-3/4 mx-auto h-auto pt-24 flex flex-row items-start justify-around  max-w-7xl">
           <img
             className="w-64 sm:w-96"
             src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/neww.png"
@@ -26,7 +26,7 @@ export default function HaircutforMenandWomen() {
         </div>
       </div>
       <div className="bg-black ">
-        <div className="bg-transparent w-full sm:w-3/4 mx-auto h-auto pt-24 flex flex-row items-start justify-around  max-w-7xl">
+        <div className="bg-transparent w-full md:w-3/4 mx-auto h-auto pt-24 flex flex-row items-start justify-around  max-w-7xl">
           <div className="text-right">
             <h1 className="sm:text-6xl text-4xl font-bold xl:text-8xl xl:w-3/4 ml-auto">
               <span className="text-blue-500">Men</span>&apos;s Haircut
