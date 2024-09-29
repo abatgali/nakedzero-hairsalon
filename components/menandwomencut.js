98d9500e0b2bbc8975f71ab8aca03cf908364061
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2024-09-29 13:52:31 -0400

    button and heading changes on homepage

--- components/menandwomencut.js
+++ components/menandwomencut.js
@@ -14,12 +14,12 @@ export default function HaircutforMenandWomen() {
           />
           <div>
             <h1 className="sm:text-6xl text-4xl font-bold text-left xl:text-8xl xl:w-3/4">
-              <span className="text-amber-400">Women</span>&apos;s Haircut
+              <span className="text-amber-500">Women</span>&apos;s Haircut
             </h1>
             <h2 className="text-2xl font-semibold text-left text-slate-500">
               Cut, Wash & Style
             </h2>
-            <div className="text-left text-black bg-gradient-to-r from-yellow-400 to-transparent p-4 font-bold text-2xl ">
+            <div className="text-left text-white bg-gradient-to-r from-yellow-600 to-transparent p-4 font-bold text-2xl rounded-md rounded-tl-[25px] rounded-br-[25px]">
               $45
             </div>
           </div>
@@ -29,12 +29,12 @@ export default function HaircutforMenandWomen() {
         <div className="bg-transparent w-full sm:w-3/4 mx-auto h-auto pt-24 flex flex-row items-start justify-around  max-w-7xl">
           <div className="text-right">
             <h1 className="sm:text-6xl text-4xl font-bold xl:text-8xl xl:w-3/4 ml-auto">
-              <span className="text-blue-400">Men</span>&apos;s Haircut
+              <span className="text-blue-500">Men</span>&apos;s Haircut
             </h1>
             <h2 className="text-2xl font-semibold text-slate-500">
               Cut, Wash & Style
             </h2>
-            <div className=" text-black bg-gradient-to-l from-blue-400 to-transparent p-4 font-bold text-2xl ">
+            <div className=" text-white bg-gradient-to-l from-blue-600 to-transparent p-4 font-bold text-2xl rounded-md rounded-tl-[25px] rounded-br-[25px]">
               $35
             </div>
           </div>
