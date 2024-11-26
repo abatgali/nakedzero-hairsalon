7ef998a6adc6344d9fad7135cbd85e6cfdafe8d8
Anant Batgali <anantb7@proton.me>
2024-11-26 17:07:23 -0500

    completed new Services section

--- components/ServiceBento.js
+++ components/ServiceBento.js
@@ -3,97 +3,95 @@ import styles from "./styles/Background.module.css";
 export default function ServiceBento() {
   return (
     <>
-      <div className=" py-24 sm:py-32">
+      <div className=" py-24 sm:py-32" id="services">
         <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-base/7 font-semibold text-indigo-400">
-            Services
+          <h2 className="text-base/7 font-semibold text-orange-400 mx-auto text-center">
+            The Ultimate Salon Experience
           </h2>
-          <p className="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
-            Wide Selection of Services
+          <p className="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl mx-auto text-center">
+            Trims, Tones and Treatments
           </p>
           <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-            <div className="flex p-px lg:col-span-4">
-              <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
+            <div className="flex p-px lg:col-span-3">
+              <div className="overflow-hidden rounded-lg bg-stone-800 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
                 <img
                   alt=""
-                  src="https://storage.googleapis.com/allprojectsdata/IMG_0244.PNG"
+                  src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/servicesImages/shapingService.png"
                   className="h-80 object-cover object-left"
                 />
                 <div className="p-10">
                   <h3 className="text-sm/4 font-semibold text-gray-400">
-                    Releases
+                    Starting from $40
                   </h3>
                   <p className="mt-2 text-lg font-medium tracking-tight text-white">
-                    Push to deploy
+                    Shaping & Styling
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                    gravida justo et nulla efficitur, maximus egestas sem
-                    pellentesque.
+                    Capture your unique essence with our expert hairstylists.
+                    Get the look you want with our cutting-edge cuts and styles.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex p-px lg:col-span-3">
+              <div className="overflow-hidden rounded-lg bg-stone-800 lg:rounded-tr-[2rem]">
+                <img
+                  alt=""
+                  src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/servicesImages/coloringService.png"
+                  className="h-80 object-cover"
+                />
+                <div className="p-10">
+                  <h3 className="text-sm/4 font-semibold text-gray-400">
+                    Starting from $50
+                  </h3>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-white">
+                    Hair Coloring
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
+                    Balayage, highlights, full coloring, gloss toning and more.
                   </p>
                 </div>
               </div>
             </div>
             <div className="flex p-px lg:col-span-2">
-              <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-tr-[2rem]">
+              <div className="overflow-hidden rounded-lg bg-stone-800 lg:rounded-bl-[2rem]">
                 <img
                   alt=""
-                  src="https://tailwindui.com/plus/img/component-images/bento-02-integrations.png"
+                  src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/servicesImages/treatment.jpg"
                   className="h-80 object-cover"
                 />
                 <div className="p-10">
                   <h3 className="text-sm/4 font-semibold text-gray-400">
-                    Integrations
+                    Starting from $30
                   </h3>
                   <p className="mt-2 text-lg font-medium tracking-tight text-white">
-                    Connect your favorite tools
+                    Hair & Scalp Treatments
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                    Curabitur auctor, ex quis auctor venenatis, eros arcu
-                    rhoncus massa.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="flex p-px lg:col-span-2">
-              <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-bl-[2rem]">
-                <img
-                  alt=""
-                  src="https://tailwindui.com/plus/img/component-images/bento-02-security.png"
-                  className="h-80 object-cover"
-                />
-                <div className="p-10">
-                  <h3 className="text-sm/4 font-semibold text-gray-400">
-                    Security
-                  </h3>
-                  <p className="mt-2 text-lg font-medium tracking-tight text-white">
-                    Advanced access control
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                    Vestibulum ante ipsum primis in faucibus orci luctus et
-                    ultrices posuere cubilia.
+                    NakedzerO Hair Serum, Hair Growth, Scalp Treatment, NakedzerO
+                    Luxu and more.
                   </p>
                 </div>
               </div>
             </div>
             <div className="flex p-px lg:col-span-4">
-              <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
+              <div className="overflow-hidden rounded-lg bg-stone-800 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
                 <img
                   alt=""
-                  src="https://tailwindui.com/plus/img/component-images/bento-02-performance.png"
+                  src="https://storage.googleapis.com/allprojectsdata/nakedzerosalon/servicesImages/massageService2.png"
                   className="h-80 object-cover object-left"
                 />
                 <div className="p-10">
                   <h3 className="text-sm/4 font-semibold text-gray-400">
-                    Performance
+                    Starting from $60
                   </h3>
                   <p className="mt-2 text-lg font-medium tracking-tight text-white">
-                    Lightning-fast builds
+                    Relaxation Bliss Body Massage
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                    Sed congue eros non finibus molestie. Vestibulum euismod
-                    augue vel commodo vulputate. Maecenas at augue sed elit
-                    dictum vulputate.
+                    Indulge in our expertly crafted Body Massage therapy,
+                    designed to melt away stress and leave you feeling refreshed
+                    and rejuvenated.
                   </p>
                 </div>
               </div>
